4e2363244f86359a17a3a6226f0e38e2d7d9b8f3
Basiruddin Khan <khan.basiruddin2012@gmail.com>
2022-01-16 15:58:21 +0530

    updated

--- abnwebtech/wp-content/themes/abn-webtech/header.php
+++ abnwebtech/wp-content/themes/abn-webtech/header.php
@@ -12,8 +12,11 @@
     
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> -->
     
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.theme.default.min.css">
--- abnwebtech/wp-content/themes/abn-webtech/page-templates/Contact-us.php
+++ abnwebtech/wp-content/themes/abn-webtech/page-templates/Contact-us.php
@@ -13,7 +13,7 @@
      $bannar_title=get_the_title();
  } 
 ?>
-<!--header end--> 
+<!--header end-->
 <style>
 	.page-banner {
 		background-image: url(<?php echo $bannar_img; ?>);
@@ -23,80 +23,88 @@
 		background-attachment: fixed;
 	}
 </style>
-<section class="page-banner page-banner bg-blue-red pd-tb-150">
+	<section class="breadcrumb-wrap page-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="breadcrumb-content">
+                        <h2 class="breadcrumb-title fw-bold display-5"><?php the_title(); ?> </h2>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home </a><i class="fas fa-angle-double-right"></i></li>
+                            <li class="breadcrumb-item active">FAqs</li>
+                        </ul>
+                    </div>
+                    <!-- /Breadcrumb Content -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /breadcrumb-wrap --> 
+<section class="contact-wrap sec-py">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<h2 style="color:#fff" class="text-uppercase">Let's Talk</h2>
-				<span style="color:#fff"><p class="font-weight-bold">We'd love to answer any questions you may have. Contact us and discuss your business objectives.</p></span>
-				<ol class="breadcrumb d-none">
-					<li class="breadcrumb-item"><a href="#">Home</a>
-					</li>
-					<li class="breadcrumb-item active">
-						<a href="#">Website Development Course in Kolkata</a>
-					</li>
-				</ol>
-			</div>
-		</div>
-	</div>
-</section>
-<section class="contact-us">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-10 mx-auto">
-				<div class="section-title">
-					<h3>Get In Touch Today!</h3>
+		<div class="row justify-content-center">
+			<div class="col-lg-10">
+				<div class="sec-header">
+					<h3 class="title">Get In Touch Today!</h3>
 				</div>
-				<div class="section-description text-center">
-				<p class="lead">We’re always up for a yarn, give us a call on <span class="font-weight-bold">+91 881 881 0334</span>
-				and let us know all about your business and online goals. <br>If have an email, enter your email, details using this form and we’ll be in touch soon. Can’t wait to hear from you! </p>
-					
+				<div class="sec-description text-center">
+					<p class="w-75 mx-auto">We’re always up for a yarn, give us a call on <span class="font-weight-bold">+91 881 881 0334</span>
+						and let us know all about your business and online goals. <br>If have an email, enter your email, details using this form and we’ll be in touch soon. Can’t wait to hear from you! </p>
+					<p class="font-weight-bold " id="msg">Don't hesitate to talk to us, even if it is a tiny requirement you have.</p>
 				</div>
-				<div class="row">
-					<!--   Contact Form Star  -->
-					<div class="col-md-12 text-center">
-						<p class="font-weight-bold " id="msg">Don't hesitate to talk to us, even if it is a tiny requirement you have.</p>
-					</div>
-				</div>
+
 			</div>
 		</div>			
 		<div class="row">
-			<div class="col-md-10 mx-auto contact-body">
-				<form class="contact-form" id="contact-us" action="#" method="post">
-					<div class="row">
-						<div class="col-md-6 mb-3">
-							<input type="text" class="form-control" placeholder="Fullname" value="" name="fname">
+			<div class="col-lg-5">
+				<div class="contact-info">
+					<h3 class="title">Let’s Get In Touch!</h3>
+					<p>Contact us at any time. We’d be delighted to hear from you – even if you just want to chat.</p>
+					<p>We are especially eager to talk about your website problems and/or related online marketing issues.</p>
+					<ul>
+						<li><a href="#"> <i class="fab fa-whatsapp"></i> +91 881 881 0334 / 8145 6161 91</a></li>
+						<li><a href="#"> <i class="fas fa-envelope"></i> info@abnwebtech.com</a></li>
+						<li><a href="#"> <i class="fas fa-globe"></i> https://abnwebtech.com</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-7">
+				<div class="contact-form">
+					<h3 class="title">Contact Us</h3>
+					<form id="contact-us" action="#" method="post">
+						<div class="row">
+							<div class="col-md-6 mb-3">
+								<input type="text" class="form-control" placeholder="Fullname" value="" name="fname">
+							</div>
+							<div class="col-md-6 mb-3">
+								<input type="text" name="email" class="form-control" placeholder="Email" value="">
+							</div>
 						</div>
-						<div class="col-md-6 mb-3">
-							<input type="text" name="email" class="form-control" placeholder="Email" value="">
+						<div class="row">
+							<div class="col-md-6 mb-3">
+								<input type="text" name="mobile" class="form-control" placeholder="Mobile" value="">
+							</div>
+							<div class="col-md-6 mb-3">
+								<input type="text" name="subject" class="form-control" placeholder="Subject">						
+							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6 mb-3">
-							<input type="text" name="mobile" class="form-control" placeholder="Mobile" value="">
+						<div class="row">
+							<div class="col-md-12  mb-3">
+								<input type="text" name="website" class="form-control" placeholder="Website ( If you have )" value="">
+							</div>
 						</div>
-						<div class="col-md-6 mb-3">
-							<input type="text" name="subject" class="form-control" placeholder="Subject">						
+						<div class="row">
+							<div class="col-md-12  mb-3">
+								<textarea class="form-control" name="message" placeholder="Write Your Message ..."></textarea>
+							</div>
 						</div>
-					
-					</div>
-
-					<div class="row">
-						<div class="col-md-12  mb-3">
-							<input type="text" name="website" class="form-control" placeholder="Website ( If you have )" value="">
+						<div class="row">
+							<div class="col-md-12">
+								<button class="btn btn-solid" type="submit"><i class="fa fa-paper-plane-o"></i> Send Message </button>
+							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12  mb-3">
-							<textarea class="form-control" name="message" placeholder="Write Your Message ..."></textarea>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12  mb-3">
-							<button class="btn btn-primary btn-lg" type="submit"><i class="fa fa-paper-plane-o"></i> Send Message </button>
-						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
 		<!--   Contact Form End  -->
